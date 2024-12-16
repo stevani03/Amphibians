2054cae30ea84cf69112f1d1bc181358d140839d
stevani03 <147477260+stevani03@users.noreply.github.com>
2024-12-16 21:38:33 +0700

    Update MainActivity.kt

--- app/src/main/java/id/stevani/amphibians/MainActivity.kt
+++ app/src/main/java/id/stevani/amphibians/MainActivity.kt
@@ -1,4 +1,4 @@
-package id.stevani.amphibians
+package id.stevani.amphibians // Menentukan nama paket untuk file Kotlin ini.
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -10,17 +10,22 @@ import androidx.compose.ui.Modifier
 import id.stevani.amphibians.ui.AmphibiansApp
 import id.stevani.amphibians.ui.theme.AmphibiansTheme
 
+// Kelas utama aplikasi, yaitu MainActivity, yang merupakan turunan dari ComponentActivity.
 class MainActivity : ComponentActivity() {
+    // Metode onCreate yang akan dipanggil ketika aktivitas dibuat.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Fungsi setContent digunakan untuk mengatur antarmuka pengguna aplikasi menggunakan Jetpack Compose.
         setContent {
+            // Menggunakan tema aplikasi khusus, yaitu AmphibiansTheme.
             AmphibiansTheme {
-                // A surface container using the 'background' color from the theme
+                // Surface adalah container dengan warna latar belakang sesuai tema.
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), // Mengatur ukuran Surface agar memenuhi layar.
+                    color = MaterialTheme.colorScheme.background // Menggunakan warna latar belakang dari skema tema material.
                 ) {
-                    AmphibiansApp()
+                    AmphibiansApp() // Memanggil fungsi AmphibiansApp untuk menampilkan UI aplikasi.
                 }
             }
         }
