c8128fa98025a498506e1ba4bd6894c410a830a6
stevani03 <147477260+stevani03@users.noreply.github.com>
2024-12-16 21:38:50 +0700

    Update AmphibiansApplication.kt

--- app/src/main/java/id/stevani/amphibians/AmphibiansApplication.kt
+++ app/src/main/java/id/stevani/amphibians/AmphibiansApplication.kt
@@ -5,7 +5,7 @@ import id.stevani.amphibians.data.AppContainer
 import id.stevani.amphibians.data.DefaultAppContainer
 
 class AmphibiansApplication : Application() {
-    /** AppContainer instance used by the rest of classes to obtain dependencies */
+  
     lateinit var container: AppContainer
     override fun onCreate() {
         super.onCreate()
