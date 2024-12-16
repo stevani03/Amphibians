4728275513b5f995b13fd7641a9fd99f0e287bd4
stevani03 <147477260+stevani03@users.noreply.github.com>
2024-12-16 21:35:48 +0700

    Update AmphibiansApiService.kt

--- app/src/main/java/id/stevani/amphibians/network/AmphibiansApiService.kt
+++ app/src/main/java/id/stevani/amphibians/network/AmphibiansApiService.kt
@@ -3,7 +3,13 @@ package id.stevani.amphibians.network
 import id.stevani.amphibians.model.Amphibian
 import retrofit2.http.GET
 
+
+// Interface untuk layanan API Retrofit.
+// Mendefinisikan endpoint yang digunakan untuk mengambil data dari server.
 interface AmphibiansApiService {
-    @GET("amphibians")
-    suspend fun getAmphibians(): List<Amphibian>
+
+// Mendefinisikan endpoint untuk mengambil daftar Amphibian dari server.
+// Menggunakan anotasi @GET untuk menentukan metode HTTP yang digunakan (GET).
+@GET("amphibians")
+suspend fun getAmphibians(): List<Amphibian>
 }
